748e7e51fa7e06674c6a42efce121c90ce46374d
Cesar-M-Diaz <arq.cmdiaz1@gmail.com>
2023-01-14 15:51:47 +0100

    fix: make the url a env variable

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,13 +4,15 @@ const errorValidator = require('../utils/validationErr')
 const { v4: uuidv4 } = require('uuid')
 const bcrypt = require('bcrypt')
 
+const urlName = process.env.URLNAME
+
 function routes (fastify, options, done) {
   fastify.get('/welcome', { onRequest: [fastify.authenticate] }, async (req, reply) => {
     await reply.code(200).view('home.hbs', { scriptN: reply.cspNonce.script, styleN: reply.cspNonce.style })
   })
 
   fastify.get('/', { onRequest: [fastify.authenticate] }, async (req, reply) => {
-    await reply.code(200).view('shortener.hbs', { title: 'Shorten a url', scriptN: reply.cspNonce.script, styleN: reply.cspNonce.style })
+    await reply.code(200).view('shortener.hbs', { title: 'Shorten a url', scriptN: reply.cspNonce.script, styleN: reply.cspNonce.style, urlName })
   })
 
   fastify.get('/register', { onRequest: [fastify.authenticate] }, async (req, reply) => {
@@ -32,8 +34,7 @@ function routes (fastify, options, done) {
         'SELECT id, url FROM urls WHERE user_id=$1', [userId]
       )
 
-      const hbsObj = rows.length > 0 ? { urls: rows, ...nonce } : { ...nonce }
-
+      const hbsObj = rows.length > 0 ? { urls: rows, ...nonce, urlName } : { ...nonce }
       return reply.code(200).view('user.hbs', hbsObj)
     } catch (err) {
       return reply.send(err)
@@ -139,13 +140,13 @@ function routes (fastify, options, done) {
       )
       if (rows.length > 0) {
         // 409: conflict, This response is sent when a request conflicts with the current state of the server. (client error)
-        return reply.code(409).view('shortener.hbs', { title: 'You already shortened this url', shortenedUrl: `${rows[0].id}`, ...nonce })
+        return reply.code(409).view('shortener.hbs', { title: 'You already shortened this url', shortenedUrl: `${rows[0].id}`, ...nonce, urlName })
       }
       const randomId = (Math.random() + 1).toString(36).substring(7)
       const inserted = await client.query(
         'INSERT INTO urls(id, url, user_id) VALUES($1, $2, $3) RETURNING *', [randomId, urlString, userId]
       )
-      return reply.code(201).view('shortener.hbs', { title: 'Heres your shortened url', shortenedUrl: `${inserted.rows[0].id}`, ...nonce })
+      return reply.code(201).view('shortener.hbs', { title: 'Heres your shortened url', shortenedUrl: `${inserted.rows[0].id}`, ...nonce, urlName })
     } catch (e) {
       return reply.send(e)
     } finally {
@@ -192,7 +193,7 @@ function routes (fastify, options, done) {
       const { rows } = await client.query(
         'SELECT id, url FROM urls WHERE user_id=$1', [userId]
       )
-      return reply.code(200).view('user.hbs', { urls: rows, ...nonce })
+      return reply.code(200).view('user.hbs', { urls: rows, ...nonce, urlName })
     } catch (e) {
       return reply.send(e)
     } finally {
