35d0591623c0953e5d9ebd540c57631068bc8cac
Cesar-M-Diaz <arq.cmdiaz1@gmail.com>
2023-04-26 12:07:58 +0200

    add more tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,12 +3,47 @@
 const { test } = require('tap')
 const build = require('../src/app')
 
-test('requests the "/" route', async t => {
+test('requests the "/welcome" route', async t => {
   const app = build()
+
+  t.teardown(() => {
+    app.close()
+  })
 
   const response = await app.inject({
     method: 'GET',
-    url: '/'
+    url: '/welcome'
   })
-  t.equal(response.statusCode, 302, 'returns a status code of 302')
+
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+})
+
+test('requests the "/register" route', async t => {
+  const app = build()
+
+  t.teardown(() => {
+    app.close()
+  })
+
+  const response = await app.inject({
+    method: 'GET',
+    url: '/register'
+  })
+
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+})
+
+test('requests the "/login" route', async t => {
+  const app = build()
+
+  t.teardown(() => {
+    app.close()
+  })
+
+  const response = await app.inject({
+    method: 'GET',
+    url: '/login'
+  })
+
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
 })
